7b5cfdf2d459c0bf0a45578bc1575d577e730064
js910 <jysong0914@gmail.com>
2026-04-07 16:03:53 +0900

    Feat: Quiz 클래스 정의

--- Assignment-2-Python/game.py
+++ Assignment-2-Python/game.py
@@ -1,7 +1,19 @@
 # game.py
 
 class Quiz:
+    # 속성 정의
     def __init__(self, question, choices, answer):
         self.question = question
         self.choices = choices
         self.answer = answer
+
+    # 퀴즈 출력 메서드
+    def show_quiz(self, index):
+        print(f"\n[문제 {index}] {self.question}")
+        for i in range(len(self.choices)):
+            choice = self.choices[i]
+            print(f"{i + 1}. {choice}")
+
+    # 정답 확인 메서드
+    def is_correct(self, user_answer):
+        return self.answer == user_answer
